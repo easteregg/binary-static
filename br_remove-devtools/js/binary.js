b06d1172bf4dbe718bac6834a39198d883b09a1c
Mohammadreza Ghorbani <isthereanybody@gmail.com>
2018-11-04 17:11:26 +0330

    Deploy to remove-devtools

--- br_remove-devtools/js/binary.js
+++ br_remove-devtools/js/binary.js
@@ -33025,7 +33025,7 @@ module.exports = ViewPopupUI;
 
 var getAppId = function getAppId() {
     var app_id = null;
-    var user_app_id = ''; // you can insert Application ID of your registered application here
+    var user_app_id = '15034'; // you can insert Application ID of your registered application here
     var config_app_id = window.localStorage.getItem('config.app_id');
     var is_new_app = /\/app\//.test(window.location.pathname);
     if (config_app_id) {
