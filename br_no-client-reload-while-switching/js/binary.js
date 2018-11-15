9bf8bf39d3eb59b6816de336a798e5113555505d
Mohammadreza Ghorbani <isthereanybody@gmail.com>
2018-11-15 12:05:10 +0330

    Deploy to no-client-reload-while-switching

--- br_no-client-reload-while-switching/js/binary.js
+++ br_no-client-reload-while-switching/js/binary.js
@@ -609,6 +609,7 @@ var ClientBase = function () {
         setNewAccount: setNewAccount,
         currentLandingCompany: currentLandingCompany,
         shouldCompleteTax: shouldCompleteTax,
+        getAllAccountsObject: getAllAccountsObject,
         getMT5AccountType: getMT5AccountType,
         getBasicUpgradeInfo: getBasicUpgradeInfo,
         getLandingCompanyValue: getLandingCompanyValue,
@@ -864,6 +865,7 @@ var GTM = function () {
         var req = response.echo_req.passthrough;
         var data = {
             event: 'buy_contract',
+            bom_ui: 'legacy',
             bom_symbol: req.symbol,
             bom_market: getElementById('contract_markets').value,
             bom_currency: req.currency,
@@ -9228,6 +9230,7 @@ module.exports = BinaryPjax;
 var BinarySocket = __webpack_require__(/*! ./socket */ "./src/javascript/app/base/socket.js");
 var RealityCheckData = __webpack_require__(/*! ../pages/user/reality_check/reality_check.data */ "./src/javascript/app/pages/user/reality_check/reality_check.data.js");
 var ClientBase = __webpack_require__(/*! ../../_common/base/client_base */ "./src/javascript/_common/base/client_base.js");
+var GTM = __webpack_require__(/*! ../../_common/base/gtm */ "./src/javascript/_common/base/gtm.js");
 var SocketCache = __webpack_require__(/*! ../../_common/base/socket_cache */ "./src/javascript/_common/base/socket_cache.js");
 var getElementById = __webpack_require__(/*! ../../_common/common_functions */ "./src/javascript/_common/common_functions.js").getElementById;
 var urlLang = __webpack_require__(/*! ../../_common/language */ "./src/javascript/_common/language.js").urlLang;
@@ -9297,7 +9300,11 @@ var Client = function () {
         if (show_login_page) {
             sessionStorage.setItem('showLoginPage', 1);
         }
-        BinarySocket.send({ logout: '1' });
+        BinarySocket.send({ logout: '1' }).then(function (response) {
+            if (response.logout === 1) {
+                GTM.pushDataLayer({ event: 'log_out' });
+            }
+        });
     };
 
     var doLogout = function doLogout(response) {
