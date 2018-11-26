342d3b12023c4e0bece9e00c62ecf5c96f078bdf
Mohammadreza Ghorbani <isthereanybody@gmail.com>
2018-11-26 11:35:21 +0330

    Deploy to redirect-to-trading-on-click

--- br_redirect-to-trading-on-click/app/js/binary.js
+++ br_redirect-to-trading-on-click/app/js/binary.js
@@ -13973,8 +13973,8 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var EmptyStatementMessage = function (_Component) {
-    _inherits(EmptyStatementMessage, _Component);
+var EmptyStatementMessage = function (_PureComponent) {
+    _inherits(EmptyStatementMessage, _PureComponent);
 
     function EmptyStatementMessage(props) {
         _classCallCheck(this, EmptyStatementMessage);
@@ -14025,7 +14025,7 @@ var EmptyStatementMessage = function (_Component) {
     }]);
 
     return EmptyStatementMessage;
-}(_react.Component);
+}(_react.PureComponent);
 
 EmptyStatementMessage.propTypes = {
     has_selected_date: _propTypes2.default.bool
