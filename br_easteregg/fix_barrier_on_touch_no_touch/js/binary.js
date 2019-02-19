087abf48bb826c5e3d633903e4fc3c9214015726
Mohammadreza Ghorbani <isthereanybody@gmail.com>
2019-02-19 16:39:39 +0330

    Deploy to easteregg/fix_barrier_on_touch_no_touch

--- br_easteregg/fix_barrier_on_touch_no_touch/js/binary.js
+++ br_easteregg/fix_barrier_on_touch_no_touch/js/binary.js
@@ -200,7 +200,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/binary-static/br_easteregg/fix_barrier_on_touch_no_touch/js/";
+/******/ 	__webpack_require__.p = "/br_easteregg/fix_barrier_on_touch_no_touch/js/";
 /******/
 /******/ 	// on error function for async loading
 /******/ 	__webpack_require__.oe = function(err) { console.error(err); throw err; };
