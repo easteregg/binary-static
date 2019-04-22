5ba02f84088c82cfa14e7eb293b87cc50e9d936c
Mohammadreza Ghorbani <isthereanybody@gmail.com>
2019-04-22 13:01:55 +0430

    Deploy to reports-tab

---
+++ br_reports-tab/app/js/open_positions-c54e278f39ea25b0ad74.js
@@ -0,0 +1,35 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["open_positions"],{
+
+/***/ "./src/javascript/app_2/Modules/Reports/Containers/open-positions.jsx":
+/*!****************************************************************************!*\
+  !*** ./src/javascript/app_2/Modules/Reports/Containers/open-positions.jsx ***!
+  \****************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var OpenPositions = function OpenPositions() {
+  return _react2.default.createElement(
+    'h1',
+    null,
+    'LOL2'
+  );
+};
+
+exports.default = OpenPositions;
+
+/***/ })
+
+}]);
